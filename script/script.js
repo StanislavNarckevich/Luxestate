01cfdd56d8495f6d15180b44c0151409a8b034ac
StanislavNarckevich <azrae2197@gmail.com>
2020-09-07 23:50:44 +0300

    first version

---
+++ script/script.js
@@ -0,0 +1,32 @@
+let stpbl1 = document.getElementById('step-bl1');
+let stpbrdr1 = document.getElementById('step-brdr1');
+
+stpbl1.onmouseover = function() {
+    stpbrdr1.style.borderBottom = '3px solid white'
+};
+
+stpbl1.onmouseout = function() {
+    stpbrdr1.style.borderBottom = '3px solid black'
+};
+
+let stpbl2 = document.getElementById('step-bl2');
+let stpbrdr2 = document.getElementById('step-brdr2');
+
+stpbl2.onmouseover = function() {
+    stpbrdr2.style.borderBottom = '3px solid white'
+};
+
+stpbl2.onmouseout = function() {
+    stpbrdr2.style.borderBottom = '3px solid black'
+};
+
+let stpbl3 = document.getElementById('step-bl3');
+let stpbrdr3 = document.getElementById('step-brdr3');
+
+stpbl3.onmouseover = function() {
+    stpbrdr3.style.borderBottom = '3px solid white'
+};
+
+stpbl3.onmouseout = function() {
+    stpbrdr3.style.borderBottom = '3px solid black'
+};
